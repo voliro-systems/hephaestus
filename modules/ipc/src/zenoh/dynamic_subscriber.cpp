21aec15f5a87cc7552ada55bcde7cdfbf6817ade
Marius Fehr <mbf@voliro.com>
2025-02-07 13:27:26 +0100

    fix some merge issues

--- modules/ipc/src/zenoh/dynamic_subscriber.cpp
+++ modules/ipc/src/zenoh/dynamic_subscriber.cpp
@@ -79,15 +79,14 @@ void DynamicSubscriber::onPublisherAdded(const EndpointInfo& info) {
   }
 
   if (init_subscriber_cb_) {
-    auto type_info = topic_db_->getTypeInfo(info.topic);
-
-    if (!type_info.has_value()) {
-      heph::log(heph::ERROR, "failed to get type info for topic", "topic", info.topic);
+    optional_type_info = topic_db_->getTypeInfo(info.topic);
+    if (optional_type_info.has_value()) {
+      heph::log(
+          heph::ERROR,
+          fmt::format("Failed to get type info for topic: {} - cannot call init subscriber!", info.topic));
       return;
     }
-
-    init_subscriber_cb_(info.topic, type_info.value());
-    optional_type_info = std::move(type_info);
+    init_subscriber_cb_(info.topic, optional_type_info.value());
   }
 
   heph::log(heph::DEBUG, "create subscriber", "topic", info.topic);
